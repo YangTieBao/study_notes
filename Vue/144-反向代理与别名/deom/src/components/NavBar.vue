eddbe4698df42e8375b48d0924a94c3dfc8850df
YangTieBao <2917553892@qq.com>
2023-09-21 15:29:05 +0800

    反向代理与别名

---
+++ Vue/144-反向代理与别名/deom/src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button v-show="myleft">left</button>
+        navbar-{{ myname }}
+        <button @click="handle1">right</button>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['myname', 'myleft'],
+  methods: {
+    handle1 () {
+      this.$emit('event')
+    }
+  }
+}
+</script>
---
+++ Vue/144-反向代理与别名/deom/src/components/SideBar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="data in datalist" :key="data">
+                {{ data.nm }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  mounted () {
+    // fetch('/maizuo.json')
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     console.log(res.data.films)
+    //     this.datalist = res.data.films
+    //   })
+    // axios.get('/maoyan.json').then(res => {
+    //   console.log(res.data.data.hot)
+    //   this.datalist = res.data.data.hot
+    // })
+    axios.get('/ytb/api/mmdb/movie/v3/list/hot.json?ct=%E6%B9%98%E6%BD%AD&ci=264&channelId=4').then(res => {
+      // console.log(res.data.data.hot)
+      this.datalist = res.data.data.hot
+    })
+  },
+  data () {
+    return {
+      datalist: []
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+// scoped 表示局部
+ul{
+  li{
+    background:orange;
+  }
+}
+</style>
